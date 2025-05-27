4c5353622b6b9ba2bffaedf2eed8eba0cb311c5c
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-05-27 12:01:09 -0400

    installed emailJS, service i use to deliver messages sent to client via the contact form in the contact component. set up form to catch the data entered using react hook useRef. init funciton that will handle sendign the form data using email.js

--- src/components/pageComponents/Contact.jsx
+++ src/components/pageComponents/Contact.jsx
@@ -4,12 +4,70 @@ import React from "react";
 import { CiInstagram } from "react-icons/ci";
 import { TfiEmail } from "react-icons/tfi";
 
+// React Hooks
+import { useRef } from "react";
+
+// emailJS
+import emailjs from "@emailjs/browser";
+
 // redux
 import { useSelector } from "react-redux";
 
 const Contact = () => {
   // redux & state
   const staticState = useSelector((state) => state.staticTextSlice);
+  // form state ref
+  // Initial state
+  const formRef = useRef(null);
+  const nameRef = useRef(null);
+  const subjectRef = useRef(null);
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
+
+  // handle send form function
+  const handleUserData = (e) => {
+    e.preventDefault();
+    // handleling form filling error
+    if (
+      nameRef.current.value === "" ||
+      subjectRef.current.value === "" ||
+      emailRef.current.value === "" ||
+      messageRef.current.value === ""
+    ) {
+      nameRef.current.focus();
+      // toast("*** Todos los campos son obligatorios ***", toastStyleObject());
+      alert("all fields are mandatory");
+      return;
+    } else {
+      alert("form sent");
+    }
+    // Send data
+    // emailjs
+    //   .sendForm(
+    //     "service_b3bqjep", // service ID
+    //     "template_ubefjka", // template ID
+    //     formRef.current, // form data
+    //     "7RhgiDzoqn1bPUjZk" // key
+    //   )
+    //   .then(
+    //     (result) => {
+    //       console.log("Success!! Result:", result);
+    //       nameRef.current.value = "";
+    //       subjectRef.current.value = "";
+    //       emailRef.current.value = "";
+    //       messageRef.current.value = "";
+    //       nameRef.current.focus();
+    //       toast(
+    //         "*** Tu mensaje me ha llegado. Pronto me pondré en contacto. :) ***",
+    //         toastStyleObject()
+    //       );
+    //     },
+    //     (error) => {
+    //       console.log("Failure. Error:", error);
+    //       toast(error.text, toastStyleObject());
+    //     }
+    //   );
+  };
 
   return (
     <section className="container mx-auto h-[700px]">
@@ -37,6 +95,7 @@ const Contact = () => {
         <form
           action=""
           className="h-[550px] flex flex-col items-center justify-evenly"
+          ref={formRef}
         >
           <input
             type="text"
@@ -44,6 +103,7 @@ const Contact = () => {
             name="name"
             autocomplete="off"
             className="h-10 w-3/4 text-center md:w-1/2 focus:outline-none border-b-[1px] border-b-transparent  hover:border-b-black"
+            ref={nameRef}
           />
           <input
             type="text"
@@ -51,6 +111,7 @@ const Contact = () => {
             name="subject"
             autocomplete="off"
             className="h-10 w-3/4 text-center md:w-1/2 focus:outline-none border-b-[1px] border-b-transparent hover:border-b-black"
+            ref={subjectRef}
           />
           <input
             type="text"
@@ -58,15 +119,17 @@ const Contact = () => {
             name="email"
             autocomplete="off"
             className="h-10 w-3/4 text-center md:w-1/2 focus:outline-none border-b-[1px] border-b-transparent hover:border-b-black"
+            ref={emailRef}
           />
           <textarea
             name="message"
             rows="5"
             placeholder="Message"
             className="w-3/4 text-center md:w-1/2 border-b-[1px] border-b-transparent hover:border-b-black focus:outline-none"
+            ref={messageRef}
           ></textarea>
           <button
-            onClick={""}
+            onClick={handleUserData}
             className="h-10 hover:text-slate-600 font-extrabold text-lg transition-all duration-500"
           >
             send
