1a5a21184a5c3d3deedaaff37492de96cc0a45bc
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-05-28 12:23:51 -0400

    dynamically, with the help of a span element, changed the button in the cart component so if the cart is empty, the button to checkout looks unaccesible

--- src/components/pageComponents/ShoppingCart.jsx
+++ src/components/pageComponents/ShoppingCart.jsx
@@ -18,39 +18,59 @@ const ShoppingCart = () => {
         <div className="ml-5 text-2xl p-8 md:text-center md:text-3xl">
           Matt Marotti / Shoping cart
         </div>
-        {storeState.shoppingCart.map((prod) => (
-          <div className="mx-5 mb-8">
-            <div className="flex items-center justify-between">
-              <div>
-                <p className="text-center">1</p>
-              </div>
-              <div>
-                <img
-                  src={prod.imgSrcHref}
-                  alt="prod img"
-                  className="h-[150px] p-2 rounded-xl"
-                />
-              </div>
-              <div className=" p-2 w-1/3">
-                <p className="text-center">{prod.title}</p>
-              </div>
-              <div className=" p-2"></div>
-              <div className=" p-2"> {`${prod.cost} CAD`}</div>
 
-              <div className=" p-2">
-                <button>
-                  <FaTimes />
-                </button>
+        {storeState.shoppingCart.length > 0 ? (
+          storeState.shoppingCart.map((prod) => (
+            <div key={prod.id} className="mx-5 mb-8">
+              <div className="flex items-center justify-between">
+                <div>
+                  <p className="text-center">1</p>
+                </div>
+                <div>
+                  <img
+                    src={prod.imgSrcHref}
+                    alt="prod img"
+                    className="h-[150px] p-2 rounded-xl"
+                  />
+                </div>
+                <div className="p-2 w-1/3">
+                  <p className="text-center">{prod.title}</p>
+                </div>
+                <div className="p-2"></div>
+                <div className="p-2">{`${prod.cost} CAD`}</div>
+                <div className="p-2">
+                  <button>
+                    <FaTimes />
+                  </button>
+                </div>
               </div>
             </div>
+          ))
+        ) : (
+          <div className="text-center text-lg font-semibold m-10">
+            <p className="font-light">Your shopping cart is empty</p>
           </div>
-        ))}
+        )}
+
         <div className="h-8 flex justify-between p-5 items-center border-b-[1px] border-b-black">
           <div>Subtotal</div>
           <div>$0.00 CAD</div>
         </div>
         <div className="h-[100px] mb-5 flex justify-center items-center">
-          <Link className="font-extrabold hover:text-slate-700">checkout</Link>
+          {storeState.shoppingCart.length > 0 ? (
+            <Link
+              to="/checkout"
+              className="font-extrabold hover:text-slate-700"
+            >
+              Checkout
+            </Link>
+          ) : (
+            <span className="font-extrabold text-gray-400 cursor-not-allowed">
+              Checkout
+            </span>
+          )}
+
+          {/* <Link className="font-extrabold hover:text-slate-700">checkout</Link> */}
         </div>
         <div className=" mb-10 text-center font-extrabold hover:text-slate-700">
           <Link to="/store">Continue shopping</Link>
