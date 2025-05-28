87c93fb1230a902657751ce2d0dd301e5c7d16ea
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-05-28 08:37:11 -0400

    Created the ShoppingCart component to display products from the Redux store, used useSelector to access the shoppingCart state from storeSlice, rendered each cart item with image, title, cost, and a non-functional remove button, added a basic layout with a subtotal section and checkout link, and included responsive styling using Tailwind CSS.

--- src/components/pageComponents/ShoppingCart.jsx
+++ src/components/pageComponents/ShoppingCart.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+// react icons
+import { FaTimes } from "react-icons/fa";
+
+// React Router V6
+import { Link } from "react-router-dom";
+
 // redux
 import { useSelector } from "react-redux";
 
@@ -7,12 +13,47 @@ const ShoppingCart = () => {
   // redux & state
   const storeState = useSelector((state) => state.storeSlice);
   return (
-    <div>
-      <h1>cart</h1>
-      {storeState.shoppingCart.map((prod) => (
-        <h2>{prod.title}</h2>
-      ))}
-    </div>
+    <section className="container mx-auto flex items-end lg:justify-center">
+      <div className="mt-32 w-full flex flex-col lg:w-2/3">
+        <div className="ml-5 text-2xl p-8 md:text-center md:text-3xl">
+          Shoping cart
+        </div>
+        {storeState.shoppingCart.map((prod) => (
+          <div className="mx-5 mb-8">
+            <div className="flex items-center justify-between">
+              <div>
+                <p className="text-center">1</p>
+              </div>
+              <div>
+                <img
+                  src={prod.imgSrcHref}
+                  alt="prod img"
+                  className="h-[150px] p-2 rounded-xl"
+                />
+              </div>
+              <div className=" p-2 w-1/3">
+                <p className="text-center">{prod.title}</p>
+              </div>
+              <div className=" p-2"></div>
+              <div className=" p-2"> {prod.cost}</div>
+
+              <div className=" p-2">
+                <button>
+                  <FaTimes />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="h-8 flex justify-between p-5 items-center border-b-[1px] border-b-black">
+          <div>Subtotal</div>
+          <div>$0.00 CAD</div>
+        </div>
+        <div className="h-[100px] mb-10 flex justify-center items-center">
+          <Link className="font-extrabold hover:text-slate-700">checkout</Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
