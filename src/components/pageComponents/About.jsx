c3678be03a294f5c536db5a543777ee35ddc576a
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2025-11-20 12:35:23 -0500

    d

--- src/components/pageComponents/About.jsx
+++ src/components/pageComponents/About.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 // redux
+// redux hooks
 import { useSelector } from "react-redux";
 
+// About function component
+// About function component
+// About function component
 const About = () => {
-  // redux
+  // redux hooks and state
+  // state in static text slice
   const staticState = useSelector((state) => state.staticTextSlice);
 
+  // return
+  // return
+  // return
   return (
+    // main container
     <section className="container mx-auto  flex items-end">
       <div className="mt-32 w-full flex flex-col items-center lg:flex-row lg:justify-evenly">
+        {/* img container */}
         <div className="mt-8 h-96">
           <img
             src={staticState.about.aboutImgSrcHref}
@@ -17,8 +27,11 @@ const About = () => {
             className="h-full rounded-xl shadow-2xl"
           />
         </div>
+        {/* text container */}
         <div className="w-full lg:w-1/2 flex flex-col items-center">
+          {/* title */}
           <h1 className="text-3xl text-center mt-8">Matt Marotti</h1>
+          {/* body */}
           <p className="w-3/4 text-justify mt-8">
             {staticState.about.aboutText.slice(0, 201)}
             <br />
@@ -28,9 +41,11 @@ const About = () => {
             <br />
             {staticState.about.aboutText.slice(388)}
           </p>
+          {/* CV link */}
           <a
             href={staticState.about.cvSrcHref}
             target="_blank"
+            rel="noopener noreferrer"
             className="mt-8 mb-8 bg-black text-white rounded-lg p-3 hover:text-gray-500"
           >
             Download my CV
